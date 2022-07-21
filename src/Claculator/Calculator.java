4cadfce5469240871c450825b373c77c75915f80
REDDYN6 <naveen_reddy_atos_syntel@whirlpool.com>
2022-07-21 17:26:04 +0530

    Practice-1

---
+++ src/Claculator/Calculator.java
@@ -0,0 +1,11 @@
+package Claculator;
+
+public class Calculator {
+    public Calculator(){
+
+    }
+  protected  int divide(int a, int b){
+        System.out.println(a+b);
+        return a/b;
+    }
+}
